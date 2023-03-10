388f3a285dd876aa6f993b34d8fc9c05a19a7a71
Anton Bachin <antonbachin@yahoo.com>
2023-03-10 14:08:24 +0300

    Rename modules for vendoring in Dream

--- lwt/gluten_lwt_intf.ml
+++ lwt/gluten_lwt_intf.ml
@@ -52,6 +52,8 @@ module type IO = sig
 end
 
 module type Server = sig
+  module Gluten = Dream_gluten.Gluten
+
   type socket
   type addr
 
@@ -74,6 +76,8 @@ module type Server = sig
 end
 
 module type Client = sig
+  module Gluten = Dream_gluten.Gluten
+
   type t
   type socket
 
